e6ccbfb0d6d50326a4fb608bb92bb1d16aad2ae8
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-07-16 22:17:04 +0900

    https://github.com/yunsu8781/JAVA.git

--- Quiz/src/Main.java
+++ Quiz/src/Main.java
@@ -1,4 +1,18 @@
 
-public class Main {
+import java.util.Scanner;
+public class Main { 
+	public static void main(String[] args) {
+		
+		System.out.println("원하는 구구단을 입력해주세요");
+		Scanner sc=new Scanner(System.in);
+		int dan=sc.nextInt();  
+		
+		for(int i=1; i<=9; i++) 
+		{
+			System.out.println(dan + "*" + i + "=" + (dan*i));
+		}
+		sc.close(); 
+		
+	}
 
 }
