5342e3331dca1441f8f7b0563d0a718667c3652b
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-04 20:57:40 +0900

    멘토멘티

--- x짜 별찍기/src/Main.java
+++ x짜 별찍기/src/Main.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 	
-	  char star = '*';
-	    char space = ' ';
+
 	    int a = 1, z = 5;
 	    int i=1;
 	    for(i=1;i<=5;i++){
 	     for(int j=1;j<=5;j++){
 	       if(a==j || z==j){
-	        System.out.print(star);
+	        System.out.print("*");
 	       }else{
-	        System.out.print(space);
+	        System.out.print(" ");
 	       }
 	     }
 	     a++;
--- 멘토멘티 퀴즈 7.31/src/Quiz3/Main.java
+++ 멘토멘티 퀴즈 7.31/src/Quiz3/Main.java
@@ -15,30 +15,28 @@ public class Main {
 
 		while (HP > 0) {
 			int skill = scanner.nextInt();
-			if(HP >= 100 ) {
 			switch (skill) {
 			case 1:
 				System.out.println("skill1이 실행되었습니다.");
 				HP = HP - 30;
 				System.out.println("남은 체력 : " + HP);
-				System.out.println("다시 적을 공격하세요.");
+
 				break;
 			case 2:
 				System.out.println("skill2가 실행되었습니다.");
 				HP = HP - 70;
 				System.out.println("남은 체력 : " + HP);
-				System.out.println("다시 적을 공격하세요.");
+
 				break;
 			case 3:
 				System.out.println("skill3이 실행되었습니다.");
 				HP = HP - 100;
 				System.out.println("남은 체력 : " + HP);
-				System.out.println("다시 적을 공격하세요.");
-				break;
-              
-			}
-			}else (HP )
-		System.out.println("즉었습니다.");
 
+				break;
+			}
+		}
+		System.out.println("즉었습니다.");
+		
 	}
 }
