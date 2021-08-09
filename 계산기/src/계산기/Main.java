f3988dea4930d6c20294638dc5d54b5bcb9aa9ee
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-09 18:34:13 +0900

    홀짝

---
+++ 계산기/src/계산기/Main.java
@@ -0,0 +1,29 @@
+package °è»ê±â;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int Num = scanner.nextInt();
+		int sing = scanner.nextInt();
+
+		do {			
+		    if (sign == 1) {
+				
+			}
+		} while ();
+		
+		
+		
+		}
+		
+		
+				
+		
+	}
+}
---
+++ 홀 짝 구분/src/주사위/Main.java
@@ -0,0 +1,21 @@
+package 주사위;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int Num = sc.nextInt();
+		int Remain = Num%2; 
+				
+		if (Remain == 0 ) {
+			System.out.println("짝수입니다!");
+		} if (Remain != 0) { 
+			System.out.println("홀수입니다!");		
+		}
+		
+	}
+}
+
