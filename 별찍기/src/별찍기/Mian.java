35d4b9cc835ba9cb5670fcfe09b04a81415fd94e
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-11 21:13:19 +0900

    별찍기

---
+++ 별찍기/src/별찍기/Mian.java
@@ -0,0 +1,16 @@
+package º°Âï±â;
+
+import java.util.Iterator;
+
+public class Mian {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {				
+				System.out.print("*");			
+			}
+		System.out.println("");	
+		}						
+	}
+
+}
