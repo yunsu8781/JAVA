cf8a01f4007be385c6769c1c04a1dc0c6c54368b
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-15 23:39:54 +0900

    Add files via upload

---
+++ char/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		char a1 = 'a';
+		char a2 = 97;
+		char a3 = '\u0061';
+
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		
+}
+}
--- 배열/src/배열/Main.java
+++ 배열/src/배열/Main.java
@@ -1,9 +1,11 @@
 package 배열;
 
 public class Main {
-public static void main(String[] args) {
-	
 
+	public static void main(String[] args) {
+	
+	String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+	System.out.println(weeks[3]);
 	
 }
 }
