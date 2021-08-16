ffb2f6eb2ebed8ff4a2f46f9104e458af8167a7b
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-16 22:35:12 +0900

    Add files via upload

--- Quiz/src/Quiz1/Main.java
+++ Quiz/src/Quiz1/Main.java
@@ -1,9 +1,23 @@
 package Quiz1;
 
+import java.util.Scanner;
+
 public class Main {
+
 public static void main(String[] args) {
 	
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("점수를 적어주세요 => ");
+	int Score = scanner.nextInt();
 	
-	
+	if ( Score <= 70 ) {
+		System.out.println("해당학셍의 점수 : "+ Score);
+		System.err.println("불합격입니다.");
+	} else if (Score > 70) {
+		System.out.println("해당학생의 점수 : "+ Score);
+		System.err.println("합격입니다.");
+	}
+
+
 }
 }
