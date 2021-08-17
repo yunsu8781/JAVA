034351d9cafca6125a27545e0613143560f609ef
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-17 20:12:24 +0900

    Add files via upload

--- 여울컴 코딩테스트 2021/src/문제8/Main.java
+++ 여울컴 코딩테스트 2021/src/문제8/Main.java
@@ -1,5 +1,13 @@
 package ¹®Á¦8;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+	
+	}
 
 }
