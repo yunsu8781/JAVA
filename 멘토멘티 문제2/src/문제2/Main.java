899fe52d0d7782afb9662bab59ec90fbf1347256
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-26 21:57:35 +0900

    Add files via upload

--- 멘토멘티 문제2/src/문제2/Main.java
+++ 멘토멘티 문제2/src/문제2/Main.java
@@ -11,50 +11,40 @@ public class Main {
 		int dog = scanner.nextInt();
 		
 		switch (dog) {
-		case 1:
-			System.out.println("1번은 보리 입니다");
-			break;
-		case 2:
-			System.out.println("2번은 보리 입니다");
-			break;
-		case 3:
-			System.out.println("3번은 쿠키 입니다");
-			break;
-		case 4:
-			System.out.println("4번은 쿠키 입니다");
-			break;
-		case 5:
-			System.out.println("5번은 쿠키 입니다");
-			break;
-		case 6:
-			System.out.println("6번은 땅콩 입니다");
-			break;
-		case 7:
-			System.out.println("7번은 땅콩 입니다");
-			break;
-		case 8:
-			System.out.println("8번은 땅콩 입니다");
-			break;
-		case 9:
-			System.out.println("9번은 메리 입니다");
-			break;
-		case 10:
-			System.out.println("10번은 메리 입니다");
-			break;
-		case 11:
-			System.out.println("11번은 메리 입니다");
-			break;
 		case 12:
-			System.out.println("12번은 보리 입니다");
+		case 1:
+		case 2:
+			System.out.println(dog+"번은 보리입니다.");			
 			break;
+			
+		case 3:
+		case 4:
+		case 5:
+			System.out.println(dog+"번은 쿠키입니다.");			
+			break;
+			
+		case 6:
+		case 7:
+		case 8:
+			System.out.println(dog+"번은 땅콩입니다.");			
+			break;
+		
+		case 9:
+		case 10:
+		case 11:
+			System.out.println(dog+"번은 메리입니다.");			
+			break;
+			
 		case 0:
-			System.out.println("강아지 이름");
-			System.out.println("보리 쿠키 땅콩 메리");
-			break;
+			System.out.println(dog+"번은 보리 쿠키 땅콩 메리");
+
 		default:
 			break;
+		}
+	    
+		
 		}
 		
 		
 	}
-}
+
