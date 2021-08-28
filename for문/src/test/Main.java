9be64bf02b51cb0c1541bb2a47bbe9210bb9f9b6
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-28 21:34:59 +0900

    Add files via upload

---
+++ for문/src/test/Main.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int Num = scanner.nextInt();
+		
+		for (int i =Num; i <= Num; i++) {
+			System.out.println("¾È³çÇÏ¼¼¿ä.");
+		}
+		
+		
+	}
+}
