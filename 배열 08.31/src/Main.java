51f88ce0db319adb8b8e47ea87527bf07c28d243
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-08-31 21:52:12 +0900

    Add files via upload

---
+++ 배열 08.31/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	String[] egg = new String[4];
+	egg[1] = "후라이";
+	egg[2] = "삶은계란";
+	egg[3] = "스크램블";
+	egg[4] = "계란말이";
+	
+	System.out.println(egg[2]);
+	}
+}
+ 
