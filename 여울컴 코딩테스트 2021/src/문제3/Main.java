78022febe3e770bf2a1f11d8dccc67c2879871b6
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-09-07 21:54:56 +0900

    Add files via upload

--- 여울컴 코딩테스트 2021/src/문제3/Main.java
+++ 여울컴 코딩테스트 2021/src/문제3/Main.java
@@ -6,11 +6,11 @@ public class Main {
 public static void main(String[] args) {
 	
 Scanner scanner = new Scanner(System.in);
-int num1 = scanner.nextInt();
-int num2 = scanner.nextInt();
+int score[] = new int[10];
 
+for (int i = 0; i < 10; i++) {
+	score[i] = scanner.nextInt();
+}
 
-
-	
 }
 }
