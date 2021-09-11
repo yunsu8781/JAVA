40fb03eef9a4a8de0c02539a14b0647a4fa12b41
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-09-11 22:44:21 +0900

    Add files via upload

--- 백준 if/src/알람시계/Main.java
+++ 백준 if/src/알람시계/Main.java
@@ -8,14 +8,19 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		int h = scanner.nextInt();
-		int m = scanner.nextInt();
-		int time = m-45;
+		int m = scanner.nextInt();		
 		
-		if (time < 0 ) {
-			System.out.println(h-1 + ":" + (60 + time ));
-		}else if (time >0) {
-			System.out.println(h +":"+ time );
+		if (m < 45) {
+			h = h-1;
+			m = m+(60 - 45);
+	
+		}else if (h < 0) {
+			h = 23;
+		}else if(m > 45){
+		    m = m-45;
 		}
-		
+		System.out.println(h +" "+ m);
+
 	}
-}
+   }
+
