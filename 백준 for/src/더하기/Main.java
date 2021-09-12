c2b8199774458fdfc375106d0eb9dba11ad4d142
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-09-12 21:32:25 +0900

    210912

---
+++ 백준 for/src/더하기/Main.java
@@ -0,0 +1,19 @@
+package ¥ı«œ±‚;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+				
+		for (int i = 0; i < T; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println(a+b);
+		}
+		
+	}
+}
