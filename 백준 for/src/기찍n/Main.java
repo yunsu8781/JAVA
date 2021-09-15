df7c7f39d14a1e7fbab33cfbb87fd4907a9386a8
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-09-15 16:07:39 +0900

    Add files via upload
    20210915

---
+++ 백준 for/src/기찍n/Main.java
@@ -0,0 +1,18 @@
+package ±âÂïn;
+
+import java.util.Scanner;
+public class Main {
+
+		public static void main(String[] args) {
+			
+			Scanner scanner = new Scanner(System.in);
+			int n = scanner.nextInt();
+			
+			for (int i = n; i >= 1; i--) {
+				System.out.println(i);
+			}
+			
+		}
+}
+
+
