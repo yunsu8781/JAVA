2bfb071915040d50a11354174080f9b9ea92734c
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-10-09 03:53:40 +0900

    Add files via upload

--- 백준while/src/thirdQuiz/Main.java
+++ 백준while/src/thirdQuiz/Main.java
@@ -3,24 +3,23 @@ package thirdQuiz;
 import java.util.Scanner;
 
 public class Main {
+ 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int c = 0;
-		
-		if (N < 10) {
-			while (N!=N) {
-				N = N*10;
-				N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
-	            c++;		
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+        
+		int cnt = 0;
+		int copy = N;
+        
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			cnt++;
+ 
+			if (copy == N) {
+				break;
 			}
-		} else {
-	    	while (N!=N) {
-		    	N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
-                c++;		
-		}	
 		}
-		System.out.println(c);
+		System.out.println(cnt);
 	}
-
 }
