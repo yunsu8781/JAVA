f109c202bd9565bf40136c89beed6aa79f2db74b
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2021-11-06 18:37:56 +0900

    Add files via upload

---
+++ 백준 배열/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int num = scanner.nextInt();
+		 int[] number = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			number[i] = scanner.nextInt();
+	}
+		
+		
+
+}
+}
